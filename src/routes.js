59764683295e499661fbf0104955709a7e603b2c
Italo Abreu <italodw@gmail.com>
2020-01-16 23:21:36 -0300

    Separando controller

--- src/routes.js
+++ src/routes.js
@@ -1,33 +1,7 @@
 const { Router } = require('express');
-const axios = require('axios');
-const Dev = require('./models/Dev');
-
+const DevController = require('./controllers/DevController');
 const routes = Router();
 
-routes.post('/devs', async (req, res) => {
-  const { github_username, techs, latitude, longitude } = req.body;
-
-  const response = await axios.get(`https://api.github.com/users/${github_username}`)
-
-  const { name = login , avatar_url, bio} = response.data;
-
-  const techsArray = techs.split(',').map(techs => techs.trim());
-  
-  const location = {
-    type: 'Point',
-    coordinates: [longitude, latitude],
-  };
-
-  const dev = await Dev.create({
-    name,
-    github_username,
-    bio,
-    avatar_url,
-    techs: techsArray,
-    location,
-  });
-
-  return res.json(dev);
-});
+routes.post('/devs', DevController.store);
 
 module.exports = routes;
