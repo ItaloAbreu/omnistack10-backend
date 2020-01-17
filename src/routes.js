46768bc4d65bb333fa0c7c426aaee086e4e1a275
Italo Abreu <italodw@gmail.com>
2020-01-16 22:44:37 -0300

    cadastrando dev

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,27 @@
 const { Router } = require('express');
+const axios = require('axios');
+const Dev = require('./models/Dev');
 
 const routes = Router();
 
-routes.post('/users', (req, res) => {
-  console.log(req.body);
-  return res.json({ message:  'Hello Omnistack'});
+routes.post('/devs', async (req, res) => {
+  const { github_username, techs } = req.body;
+
+  const response = await axios.get(`https://api.github.com/users/${github_username}`)
+
+  const { name = login , avatar_url, bio } = response.data;
+
+  const techsArray = techs.split(',').map(techs => techs.trim());
+  
+  const dev = await Dev.create({
+    name,
+    github_username,
+    bio,
+    avatar_url,
+    techs: techsArray,
+  })
+
+  return res.json(dev);
 });
 
 module.exports = routes;
