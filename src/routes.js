4b78098e2948f3fe4f729e4ea344f072b7adbde6
Italo Abreu <italodw@gmail.com>
2020-01-16 23:10:21 -0300

    Cadastro de Devs finalizado

--- src/routes.js
+++ src/routes.js
@@ -5,21 +5,27 @@ const Dev = require('./models/Dev');
 const routes = Router();
 
 routes.post('/devs', async (req, res) => {
-  const { github_username, techs } = req.body;
+  const { github_username, techs, latitude, longitude } = req.body;
 
   const response = await axios.get(`https://api.github.com/users/${github_username}`)
 
-  const { name = login , avatar_url, bio } = response.data;
+  const { name = login , avatar_url, bio} = response.data;
 
   const techsArray = techs.split(',').map(techs => techs.trim());
   
+  const location = {
+    type: 'Point',
+    coordinates: [longitude, latitude],
+  };
+
   const dev = await Dev.create({
     name,
     github_username,
     bio,
     avatar_url,
     techs: techsArray,
-  })
+    location,
+  });
 
   return res.json(dev);
 });
