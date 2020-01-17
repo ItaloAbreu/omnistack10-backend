3d1a915cc6cd457c8ea16f49f4c2ae806b43c444
Italo Abreu <italodw@gmail.com>
2020-01-16 23:24:32 -0300

    impedindo cadastro duplicado.

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -5,25 +5,28 @@ module.exports = {
   async store(req, res) {
     const { github_username, techs, latitude, longitude } = req.body;
   
-    const response = await axios.get(`https://api.github.com/users/${github_username}`)
-  
-    const { name = login , avatar_url, bio} = response.data;
-  
-    const techsArray = techs.split(',').map(techs => techs.trim());
+    let dev = await Dev.findOne({ github_username });
+
+    if (!dev) {
+      const response = await axios.get(`https://api.github.com/users/${github_username}`)
+      const { name = login , avatar_url, bio} = response.data;
+      const techsArray = techs.split(',').map(techs => techs.trim());
+      const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude],
+      };
     
-    const location = {
-      type: 'Point',
-      coordinates: [longitude, latitude],
-    };
-  
-    const dev = await Dev.create({
-      name,
-      github_username,
-      bio,
-      avatar_url,
-      techs: techsArray,
-      location,
-    });
+      dev = await Dev.create({
+        name,
+        github_username,
+        bio,
+        avatar_url,
+        techs: techsArray,
+        location,
+      });
+    }
+
+    
   
     return res.json(dev);
   }
